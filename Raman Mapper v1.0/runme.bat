1adb652112372d6075080acf9c9371e5447df4fe
amjadtaleb <amjadtaleb@gmail.com>
2019-06-01 21:45:44 +0200

    Add files via upload

---
+++ Raman Mapper v1.0/runme.bat
@@ -0,0 +1,15 @@
+echo off
+echo                                                                         ::
+echo                   Add necessary directories to environment path
+
+IF EXIST %USERPROFILE%\Anaconda3 SET PATH=%PATH%;%USERPROFILE%\Anaconda3;%USERPROFILE%\Anaconda3\Scripts;%USERPROFILE%\Anaconda3\Library\mingw-w64\bin;%USERPROFILE%\Anaconda3\Library\bin
+echo Done
+echo _______
+
+echo                                                                         ::
+echo                   Start BOKEH server with python connection
+echo _______
+
+echo                                                                         ::
+bokeh serve  --show bokeh_app.py
+rem bokeh serve  --show temp.py
